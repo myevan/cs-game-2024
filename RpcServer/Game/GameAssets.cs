02397bfa1d9210d40cb99bb5a7e27bb57ec249bb
Song Young-Jin <myevan@npixel.co.kr>
2024-09-19 10:27:40 +0900

    게임 데이터 구조

---
+++ RpcServer/Game/GameAssets.cs
@@ -0,0 +1,11 @@
+﻿namespace RpcServer.Game.Data
+{
+    public class GameProtoAsset
+    {
+        public List<HeroProto> HeroProtoList { get; set; } = new();
+
+        public Dictionary<int, int> HeroProtoNumToRow { get; set; } = new();
+
+        public Dictionary<string, int> HeroProtoDivToRow { get; set; } = new();
+    }
+}
---
+++ RpcServer/Game/SingletonServices/GameDataService.cs
@@ -0,0 +1,9 @@
+﻿using RpcServer.Game.Data;
+
+namespace RpcServer.Game.SingletonServices
+{
+    public class GameDataService
+    {
+        private GameProtoAsset _protoAsset = new(); // TODO: 생성자 로딩
+    }
+}
